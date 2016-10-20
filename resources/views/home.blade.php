4e01fb8d1f71d79a66bef117236f84c9b94c1e9c
Mauricio <aiyell447@gmail.com>
2016-10-20 10:32:35 -0500

    segundo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,21 +6,6 @@
 <body>
 
 
-	<h1>Home</h1>
-	<header>
-
-	<h1>{{request()->is('/') ? 'Estoy en el Home' : 'no Estoy en el Home'}}</h1>
-	<!-- este metodo nos da la url del sitio con el metodo request
-	<h1>{{ request()->url()}} </h1>
-	 -->
-		<nav>
-			<a href="{{route('home')}}">inicio</a>
-			<a href="{{route('saludos','andres')}}">saludo</a>
-			<a href="{{route('contactos')}}">Contacto</a>
-			<p>hola andres {{request->url()}}</p>
-		</nav>
-	</header>
-
 
 
 </body>
